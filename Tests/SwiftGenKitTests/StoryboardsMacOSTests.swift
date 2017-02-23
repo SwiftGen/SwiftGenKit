8885a143f4a661fe699c3d64a44515061e5d60bf
David Jennes <david.jennes@gmail.com>
2017-02-23 02:02:32 +0100

    Add missing mac storyboard test (customname)

--- Tests/SwiftGenKitTests/StoryboardsMacOSTests.swift
+++ Tests/SwiftGenKitTests/StoryboardsMacOSTests.swift
@@ -64,4 +64,18 @@ class StoryboardsMacOSTests: XCTestCase {
 
     XCTDiffContexts(result, expected)
   }
+  
+  func testAllStoryboardsWithCustomName() {
+    let parser = StoryboardParser()
+    do {
+      try parser.parseDirectory(at: Fixtures.directory(sub: .storyboardsMacOS))
+    } catch {
+      print("Error: \(error.localizedDescription)")
+    }
+    
+    let result = parser.stencilContext(sceneEnumName: "XCTStoryboardsScene", segueEnumName: "XCTStoryboardsSegue")
+    let expected = Fixtures.context(for: "customname.plist", sub: .storyboardsMacOS)
+    
+    XCTDiffContexts(result, expected)
+  }
 }
