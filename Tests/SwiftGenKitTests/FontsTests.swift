51dec9ad57f3d9ec3f1806bf3aecfddda075a7ed
David Jennes <david.jennes@gmail.com>
2017-08-18 01:34:05 +0200

    test the relative(to:) method

--- Tests/SwiftGenKitTests/FontsTests.swift
+++ Tests/SwiftGenKitTests/FontsTests.swift
@@ -7,7 +7,8 @@
 //
 
 import XCTest
-import SwiftGenKit
+@testable import SwiftGenKit
+import PathKit
 import AppKit.NSFont
 
 class FontsTests: XCTestCase {
@@ -24,5 +25,33 @@ class FontsTests: XCTestCase {
 
     let result = parser.stencilContext()
     XCTDiffContexts(result, expected: "defaults.plist", sub: .fonts)
+  }
+
+  // MARK: - Path relative(to:)
+
+  func testPathRelativeTo_UnrelatedIsNil() throws {
+    let parent = Path("/a/b/c")
+    let file = Path("/d/e/f")
+
+    XCTAssertNil(file.relative(to: parent))
+  }
+
+  func testPathRelativeTo_RelatedIsNotNil() throws {
+    let parent = Path("/a/b/c")
+    let file = Path("/a/b/c/d/e")
+
+    XCTAssertNotNil(file.relative(to: parent))
+  }
+
+  func testPathRelativeTo_ResultIsNotFullPath() throws {
+    let parent = Path("a/b/c")
+    let absoluteParent = parent.absolute()
+    let file = Path("a/b/c/d/e")
+    let absoluteFile = file.absolute()
+
+    XCTAssertEqual(file.relative(to: parent), "d/e")
+    XCTAssertEqual(file.relative(to: absoluteParent), "d/e")
+    XCTAssertEqual(absoluteFile.relative(to: parent), "d/e")
+    XCTAssertEqual(absoluteFile.relative(to: absoluteParent), "d/e")
   }
 }
